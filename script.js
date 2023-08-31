ea3ca4bc51c076c97246c53f02278dd6f98b0f37
JASHOBANTA KUMAR SA <109386507+jashobantasa1999@users.noreply.github.com>
2023-08-31 23:21:42 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+var timer=60;
+var score=0;
+var rn=0;
+function increaseScore(){
+    score +=10;
+    document.querySelector("#scoreval").textContent=score;
+}
+function getNewhit(){
+    rn=Math.floor(Math.random()*10);
+    document.querySelector("#hitval").textContent=rn;
+}
+function makebubble(){
+    var clutter="";
+for(var i=1;i<=140;i++){
+    var rn=Math.floor(Math.random()*10);
+    clutter +=`<div class="bubble">${rn}</div>`;
+}
+document.querySelector("#pbtm").innerHTML=clutter;
+}
+function runTimer(){
+    var timerint=setInterval(function(){
+        if(timer >0)
+        {
+            timer--;
+            document.querySelector("#timerval").textContent=timer;
+        }
+        else{
+            clearInterval(timerint);
+            document.querySelector("#pbtm").innerHTML=`<h2 style="color=blue">Game Over</h2>`;
+            
+        }
+    },1000);
+
+}
+document.querySelector("#pbtm").addEventListener("click",function(dets){
+    var clickednum=Number(dets.target.textContent);
+    if(clickednum===rn)
+    {
+        increaseScore();
+        getNewhit();
+        makebubble();
+    }
+
+})
+
+runTimer();
+makebubble();
+getNewhit();
